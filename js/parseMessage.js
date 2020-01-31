0a70b30f4277abccdc9c54fb8445c73d21fdb054
SebOuellette <bigbrosebas@gmail.com>
2020-01-31 18:19:20 -0500

    Code blocks and inline blocks

--- js/parseMessage.js
+++ js/parseMessage.js
@@ -6,6 +6,8 @@ let parseMessage = (text) => {
     textContent = textContent.replace(/\*\*(.*?)\*\*/gm, '<strong>$1</strong>');
     textContent = textContent.replace(/__(.*?)__/gm, '<u>$1</u>');
     textContent = textContent.replace(/\*(.*?)\*/gm, '<i>$1</i>');
+    textContent = textContent.replace(/```(.*?)```/gs, '<div class="codeBlock">$1</div>');
+    textContent = textContent.replace(/`(.*?)`/gm, '<span class="inlineCodeBlock">$1</span>');
 
     return textContent;
 };
