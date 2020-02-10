3177f08809fb89152d8bfa672e17b896c39f1f07
dcrime <ppp.dddd7@gmail.com>
2020-02-10 08:02:19 +0100

    Browser popup
    Links pop up in the browser instead of opening in LiveBot

--- js/app.js
+++ js/app.js
@@ -8,6 +8,33 @@ let selectedChatDiv;
 let oldimg;
 let barry = false;
 
+let shell = require('electron').shell
+document.addEventListener('click', function(event) {
+    if (event.target.tagName === 'A' && event.target.href.startsWith('http')) {
+        event.preventDefault()
+        shell.openExternal(event.target.href)
+    }
+})
+
+function scale(w, h) {
+    if (w <= 400 && h <= 400) {
+        return [w, h];
+    }
+
+    if (w == h) {
+        w = 400;
+        h = 400;
+    } else if (w > h) {
+        h = Math.round((h * 400) / w);
+        w = 400;
+    } else {
+        w = Math.round((w * 400) / h);
+        h = 400;
+    }
+    return [w, h]
+}
+
+
 // Create the app and attach event listeners
 function create() {
     document.getElementById("msgbox")
@@ -48,14 +75,14 @@ function typing() {
 
 // Options on the right pane
 function options(type, content) {
-    switch(type) {
+    switch (type) {
         case 'username':
             bot.user.setUsername(content);
             document.getElementById('userCardName').innerHTML = content;
-        break;
+            break;
 
         case 'invite':
-            selectedChan.createInvite().then(invite => {command('Created invite for '+invite.guild.name+' \nhttps://discord.gg/'+invite.code);})
-        break;
+            selectedChan.createInvite().then(invite => { command('Created invite for ' + invite.guild.name + ' \nhttps://discord.gg/' + invite.code); })
+            break;
     }
 }
