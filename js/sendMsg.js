22fca80dbf42edd750d8214400f8986934c17cbc
dcrime <ppp.dddd7@gmail.com>
2020-02-10 08:09:33 +0100

    More like discord
    Added unflip, lenny, me (Makes the text italic) deleting multiple messages through a command

--- js/sendMsg.js
+++ js/sendMsg.js
@@ -1,52 +1,88 @@
 let helpMsg = [
-        'Here is a list of available commands. \n',
-        '/help - Lists all commands.',
-        '/shrug - Prepends ¯\\_(ツ)_/¯ to your message.',
-        '/tableflip - Prepends (╯°□°）╯︵ ┻━┻ to your message.',
-        '/ping - Check the hearbeat to discord.',
-        '/server - Get some info about the server.',
-        '/eval - Execute a command.'
-    ].join('\n')
-  
+    'Here is a list of available commands. \n',
+    '/help - Lists all commands.',
+    '/shrug - Prepends ¯\\_(ツ)_/¯ to your message.',
+    '/tableflip - Prepends (╯°□°）╯︵ ┻━┻ to your message.',
+    '/unflip - Prepends (╯°□°）╯︵ ┻━┻  to your message.',
+    '/lenny - Prepends ( ͡° ͜ʖ ͡°) to your message.',
+    '/me - Displays text with emphasis',
+    '/ping - Check the hearbeat to discord.',
+    '/delete - Deletes 1 message if there is no number',
+    '/server - Get some info about the server.',
+    '/eval - Execute a command.'
+].join('\n')
+
 // Commands  
 function sendmsg() {
     if (selectedChan) {
         let text = document.getElementById('msgbox').value;
-        if (text.substring(0,1) == '/') {
+        if (text.substring(0, 1) == '/') {
             let cmd = text.split(' ')[0].substring(1);
             let msg = text.split(' ').splice(1).join(' ');
-            
+
             switch (cmd) {
                 case 'help':
                     command(helpMsg);
-                break;
-        
+                    break;
+
                 case 'shrug':
-                    selectedChan.send('¯\\_(ツ)_/¯ '+msg);
+                    selectedChan.send('¯\\_(ツ)_/¯ ' + msg);
                     document.getElementById('msgbox').value = '';
-                break;
-        
+                    break;
+
                 case 'tableflip':
-                    selectedChan.send('(╯°□°）╯︵ ┻━┻ '+msg);
+                    selectedChan.send('(╯°□°）╯︵ ┻━┻ ' + msg);
                     document.getElementById('msgbox').value = '';
-                break;
-        
+                    break;
+
+                case 'unflip':
+                    selectedChan.send('┬─┬ ノ( ゜-゜ノ)' + msg);
+                    document.getElementById('msgbox').value = '';
+                    break;
+
+                case 'lenny':
+                    selectedChan.send('( ͡° ͜ʖ ͡°) ' + msg);
+                    document.getElementById('msgbox').value = '';
+                    break;
+
+                case 'me':
+                    selectedChan.send('_' + msg + '_');
+                    document.getElementById('msgbox').value = '';
+                    break;
+
                 case 'ping':
-                    command('🏓 | Pong! The heartbeat is '+Math.round(bot.ping)+'ms.');
-                break;
-        
+                    command('🏓 | Pong! The heartbeat is ' + Math.round(bot.ping) + 'ms.');
+                    break;
+
+                case 'delete':
+                    if (msg.length == 0) {
+                        selectedChan.bulkDelete(1)
+                        command("1 message has been deleted")
+                        document.getElementById('msgbox').value = '';
+                        break;
+                    } else {
+                        var reg = /^\d+$/;
+                        if (!reg.test(msg) || msg.length > 3 || msg.content > 400) {
+                            command('It must be a number between 1 to 400')
+                            break;
+                        }
+                        selectedChan.bulkDelete(msg)
+                        command(msg + msg.content == 1 ? " message has been deleted" : " messages have been deleted")
+                        break;
+                    }
+
                 case 'server':
                     let serverinfo = [
-                            'Here is some info about '+selectedChan.guild.name+'. \n',
-                            'Members - '+selectedChan.guild.memberCount,
-                            'Channels - '+selectedChan.guild.channels.size,
-                            'Roles - '+selectedChan.guild.roles.size,
-                            'Id - '+selectedChan.guild.id,
-                            'Owner - '+selectedChan.guild.owner.user.tag
-                        ].join('\n');
+                        'Here is some info about ' + selectedChan.guild.name + '. \n',
+                        'Members - ' + selectedChan.guild.memberCount,
+                        'Channels - ' + selectedChan.guild.channels.size,
+                        'Roles - ' + selectedChan.guild.roles.size,
+                        'Id - ' + selectedChan.guild.id,
+                        'Owner - ' + selectedChan.guild.owner.user.tag
+                    ].join('\n');
                     command(serverinfo);
-                break;
-        
+                    break;
+
                 case 'eval':
                     try {
                         command(`📥 Eval \n ${msg} \n\n 📤 Output \n ${eval(msg)}`);
@@ -54,7 +90,7 @@ function sendmsg() {
                         command(`📥 Eval \n ${msg} \n\n 📤 Output \n ${err}`);
                     }
                     document.getElementById('msgbox').value = '';
-                break;
+                    break;
             }
         } else {
             // Make a new message with the sent text
