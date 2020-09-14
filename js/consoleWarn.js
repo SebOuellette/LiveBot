49789f94b65a1bdcf3f74c571361facfebf93916
Splatterxl <67709748+Splatterxl@users.noreply.github.com>
2020-09-14 20:15:42 +0100

    made a warning like the one Discord has.

---
+++ js/consoleWarn.js
@@ -0,0 +1,3 @@
+console.log("%cHold Up!", "font-size:2cm;color:red;");
+console.log("%cIf someone told you to paste something here, there is a 11/10 chance you're being scammed. Unless you really know what you're doing, close this window and stay safe.", "color:black;background:white;");
+console.log("If you really know what you're doing, then why don't you join us and contribute to this project at https://github.com/SebOulette/LiveBot/\?")
