fcebed8af6e9fcef6aa0e5c2d5fc0e2654b96f15
Splatterxl <67709748+Splatterxl@users.noreply.github.com>
2020-09-22 20:12:09 +0100

    i feel like my work here is done.

--- js/consoleWarn.js
+++ js/consoleWarn.js
@@ -1,3 +1,8 @@
-console.log("%cHold Up!", "font-size:2cm;color:red;");
-console.log("%cIf someone told you to paste something here, there is a 11/10 chance you're being scammed. Unless you really know what you're doing, close this window and stay safe.", "color:black;background:white;");
-console.log("If you really know what you're doing, then why don't you join us and contribute to this project at https://github.com/SebOulette/LiveBot/\?")
+let mynameisjeff = 0
+setInterval(function () {
+    mynameisjeff++
+    if (mynameisjeff === 1) { } else return;
+    console.log("%cHold Up!", "font-size:2cm;color:red;");
+    console.log("%cIf someone told you to paste something here, there is a 11/10 chance you're being scammed. Unless you really know what you're doing, close this window and stay safe. Please, close this window.", "color:black;background:white;");
+    console.log("If you really know what you're doing, then why don't you join us and contribute to this project at https://github.com/SebOulette/LiveBot/\?")
+}, 10000)
