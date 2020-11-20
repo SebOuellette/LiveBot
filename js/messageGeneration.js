11a6d98dc010b5f985f987a37a3e0c5a652bede6
LukasDoesDev <lukasdoesdev@gmail.com>
2020-11-20 13:47:23 +0200

    🎨 Clean up messageGeneration.js

--- js/messageGeneration.js
+++ js/messageGeneration.js
@@ -31,11 +31,11 @@ function findTimeDiff(m, previousMessage, count) {
     return [bunch, timebunch];
 }
 
-function generateMsgHTML(m, previousMessage, count = -1, fetchSize = undefined) {
+function generateMsgHTML(message, previousMessage, count = -1, fetchSize = undefined) {
     // Check if the messages should be grouped or not
     let result = [false, false];
     if (count == -1 || (count > 2 && count <= fetchSize)) {
-        result = findTimeDiff(m, previousMessage);
+        result = findTimeDiff(message, previousMessage);
     }
     
     // The bunched values
@@ -45,96 +45,111 @@ function generateMsgHTML(m, previousMessage, count = -1, fetchSize = undefined) 
     // Create the div for the dark background
     let darkBG = document.createElement('div');
     darkBG.classList.add('messageBlock');
-    darkBG.id = m.id;
+    darkBG.id = message.id;
     
     // Create the messages
     let div;
     if (!bunch) {
-        // Create message div
-        div = document.createElement('div');
-        div.classList.add('messageCont');
-        div.classList.add(m.author.id);
-        if(m.channel.type == 'dm')
-        div.classList.add('dms');
-        if (timebunch) {
-            div.classList.add('timeSeparated');
-        }
+      // Create message div
+      div = document.createElement("div");
+      div.classList.add("messageCont");
+      div.classList.add(message.author.id);
+      if (message.channel.type == "dm") div.classList.add("dms");
+      if (timebunch) {
+        div.classList.add("timeSeparated");
+      }
 
-        // Inline message container
-        // messageContainer = document.createElement("div");
-        // messageContainer.classList.add(m.author.id);
-        // messageContainer.classList.add('inlineMsgCont');
-        // div.appendChild(messageContainer);
-        
-        // Create the dark background
-        darkBG.classList.add('firstmsg');
-        div.appendChild(darkBG);
+      // Inline message container
+      // messageContainer = document.createElement("div");
+      // messageContainer.classList.add(message.author.id);
+      // messageContainer.classList.add('inlineMsgCont');
+      // div.appendChild(messageContainer);
 
-        // Create user image
-        let img = document.createElement('img');
-        let userImg = m.author.displayAvatarURL().replace(/(size=)\d+?($| )/, '$128');
-        if (m.author.avatar && m.author.avatar.startsWith('a_')){
-            let userGif = m.author.displayAvatarURL().replace('.webp', '.gif').replace(/(size=)\d+?($| )/, '$128');
-            img.src = userGif;
-            darkBG.onmouseenter = e => {
-                img.src = userGif;
-            };
-            darkBG.onmouseleave = e => {
-                img.src = userImg;
-            };
-        }
-        img.classList.add('messageImg');
-        img.src = userImg;
-        img.height = '40';
-        img.width = '40';
-        darkBG.appendChild(img);
-        
-        // Create user's name
-        let name = document.createElement('p');
-        name.innerText = (m.member ? m.member.nickname : m.author.username) || m.author.username;
-        name.classList.add('messageUsername');
-        
-        // Find the colour of their name
-        if (m.member && m.member.roles._roles.size > 1) {
-            let color;
+      // Create the dark background
+      darkBG.classList.add("firstmsg");
+      div.appendChild(darkBG);
 
-            // Use the heighest role for their color (Since the hoist role is just the one that displays, not the one that determines the color)
-            // Display the role as white if it's black
-            color = m.member.displayHexColor;
+      // Create user image
+      let img = document.createElement("img");
+      let userImg = message.author
+        .displayAvatarURL()
+        .replace(/(size=)\d+?($| )/, "$128");
+      if (message.author.avatar && message.author.avatar.startsWith("a_")) {
+        let userGif = message.author
+          .displayAvatarURL()
+          .replace(".webp", ".gif")
+          .replace(/(size=)\d+?($| )/, "$128");
+        img.src = userGif;
+        darkBG.onmouseenter = (e) => {
+          img.src = userGif;
+        };
+        darkBG.onmouseleave = (e) => {
+          img.src = userImg;
+        };
+      }
+      img.classList.add("messageImg");
+      img.src = userImg;
+      img.height = "40";
+      img.width = "40";
+      darkBG.appendChild(img);
 
-            name.style.color = color;
-        } else {
-            name.style.color = '#fff'
-        }
+      // Create user's name
+      let name = document.createElement("p");
+      name.innerText =
+        (message.member ? message.member.nickname : message.author.username) ||
+        message.author.username;
 
-        darkBG.appendChild(name);
+      name.classList.add("messageUsername");
 
-        // Create timestamp
-        let timestamp = document.createElement('p');
-        timestamp.innerText = ' ' + m.createdAt.toLocaleString('en-US', {day:'2-digit', month:'2-digit', year:'numeric', hour:'2-digit', minute:'2-digit'});
-        timestamp.classList.add("messageTimestamp");
-        darkBG.appendChild(timestamp);
+      // Find the colour of their name
+      if (message.member && message.member.roles._roles.size > 1) {
+        let color;
+
+        // Use the heighest role for their color (Since the hoist role is just the one that displays, not the one that determines the color)
+        // Display the role as white if it's black
+        color = message.member.displayHexColor;
+
+        name.style.color = color;
+      } else {
+        name.style.color = "#fff";
+      }
+
+      darkBG.appendChild(name);
+
+      // Create timestamp
+      let timestamp = document.createElement("p");
+      timestamp.innerText =
+        " " +
+        message.createdAt.toLocaleString("en-US", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+      timestamp.classList.add("messageTimestamp");
+      darkBG.appendChild(timestamp);
     } else {
-        div = document.getElementsByClassName(m.author.id);
+        div = document.getElementsByClassName(message.author.id);
         div = div[div.length - 1];
         div.appendChild(darkBG);
     }
     
     // Prepend message text
-    if (m.cleanContent.length) {
+    if (message.cleanContent.length) {
         // Render message text
         let text = document.createElement('p');
         text.classList.add('messageText');
-        text.innerHTML = parseMessage(m.cleanContent, m, false);
+        text.innerHTML = parseMessage(message.cleanContent, m, false);
 
-        if(m.editedAt)
+        if (message.editedAt)
             text.innerHTML += '<time class="edited"> (edited)</time>';
 
         darkBG.appendChild(text);
     }
     
     // Append embeds
-    m.embeds.forEach(embed => {
+    message.embeds.forEach(embed => {
         showEmbed(embed, darkBG, m);
     });
     return div;
