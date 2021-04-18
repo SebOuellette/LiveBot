bc4465f029e5057ad9f2ef527c9c29138800a890
nearlySplat <splatterxl@outlook.ie>
2021-04-18 17:18:45 +0100

    j

--- js/parseMessage.js
+++ js/parseMessage.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const APIMessage = require("discord.js");
+const { APIMessage } = require("discord.js");
 
 let parseSend = (text) => {
   // The regex used for the emojis
